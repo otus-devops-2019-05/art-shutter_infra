635cb8d618d07be2c8a41809635dcf4de9b541b5
Artem Kamenev <artem.kamenev@myoffice.team>
2019-07-09 11:08:23 +0300

    add more module files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,8 @@
 output "app_external_ip" {
-  value = "${google_compute_instance.app.*.network_interface.0.access_config.0.nat_ip}"
+  value = "${module.app.app_external_ip}"
 }
 
 output "db_external_ip" {
-  value = "${google_compute_instance.db.*.network_interface.0.access_config.0.nat_ip}"
+  value = "${modle.db.db_external_ip}"
 }
-
-/*
-output "load-balancer_ip" {
-  value = "${google_compute_global_address.lb-ip.address}"
-}
-*/
 
